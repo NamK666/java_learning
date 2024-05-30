f8d1284893b93196205251252a4122e37d1a68f6
NamK666 <chriscole77@naver.com>
2024-05-30 09:35:54 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+    public static void main(String[] args) {
+        int a = 10; // 컴퓨터의 메모리에 이 정보가 올라감. (a는 10이다)
+        System.out.println(a); // 실행될 때 까지, 메모리에서 a를 기억하고 있는 것임.
+        System.out.println(a); // 실행될 때 까지, 메모리에서 a를 기억하고 있는 것임.
+        System.out.println(a); // 실행될 때 까지, 메모리에서 a를 기억하고 있는 것임.
+        System.out.println(a); // 실행될 때 까지, 메모리에서 a를 기억하고 있는 것임.
+
+    }
+}
+
+
