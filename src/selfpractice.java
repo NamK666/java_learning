d8cd0d06da75dd158168560982fc5c5551027776
NamK666 <chriscole77@naver.com>
2024-06-01 23:52:14 +0900

    init

--- src/selfpractice.java
+++ src/selfpractice.java
@@ -1,9 +1,19 @@
 public class selfpractice {
-    public static void main(String[] args){
-        String a = "happy";
-        String b = new String("happy");
-            System.out.println(a.equals(b)); // true
-            System.out.println(a==b); // false
-            //
-    }
+  public static void main(String[] args) {
+    String a = "happy";
+    String b = new String("happy");
+    System.out.println(a.equals(b)); // true
+    System.out.println(a == b); // false
+
+    char c = 'j';
+    char d = '1';
+    System.out.println(c);
+    System.out.println(d);
+
+    String e = "";
+    System.out.println(e);
+
+    long f = 7123214124124L;
+    System.out.println(f);
+  }
 }
