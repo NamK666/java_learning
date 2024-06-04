0b6dfc3fba24ccf239faab414c0bc597599f5894
NamK666 <chriscole77@naver.com>
2024-06-04 21:38:52 +0900

    init

--- src/Practice/selfpractice1.java
+++ src/Practice/selfpractice1.java
@@ -15,34 +15,32 @@ public class selfpractice1 {
     }
 
     // 1부터 100까지의 합 (while문 사용) //
-      int i = 1;
+    int i = 1;
     int n = 100;
     int sum = 0;
-    while (i <= 100){
-        sum = sum + i;
-        i++;
+    while (i <= 100) {
+      sum = sum + i;
+      i++;
     }
     System.out.println(sum);
 
     // 주어진 배열의 평균 출력 //
-      int[] numbers = {5, 2, 9, 1, 7, 4, 6, 3, 8};
-      int index = 0;
-      int suM = 0;
-      while (index < numbers.length){
-          suM = suM + numbers[index];
-          index++;
-      }
+    int[] numbers = {5, 2, 9, 1, 7, 4, 6, 3, 8};
+    int index = 0;
+    int suM = 0;
+    while (index < numbers.length) {
+      suM = suM + numbers[index];
+      index++;
+    }
     System.out.println(suM / numbers.length);
 
-
     System.out.println("-----------------------------------");
-      // 배열 예시 //
-    int[] number = {1,2,3,4,5};
-    for (int num : number){
+    // 배열 예시 //
+    int[] number = {1, 2, 3, 4, 5};
+    for (int num : number) {
       System.out.println(num);
     }
 
-
-
+    System.out.println("-----------------------------------");
   }
 }
---
+++ src/Practice/selfpractice2.java
@@ -0,0 +1,25 @@
+package Practice;
+
+public class selfpractice2 {
+  public static void main(String[] args) {
+
+    // while 연습문제 1 //
+    int a = 10;
+    while (a >= 0) {
+      System.out.println("현재 카운트 : " + a);
+      a--;
+    }
+    System.out.println("카운트 종료");
+
+    // while 퀴즈 1 //
+    int n = 1;
+    int sum = 0;
+    while (n < 100) {
+      sum = sum + n;
+      n++;
+    }
+    System.out.println(sum);
+
+    // while 퀴즈 2 //
+  }
+}
