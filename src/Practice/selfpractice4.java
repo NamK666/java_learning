ceca977611b7c1f37f2544a40605e31f4c89cdb7
NamK666 <chriscole77@naver.com>
2024-06-08 20:26:14 +0900

    init

---
+++ src/Practice/selfpractice4.java
@@ -0,0 +1,57 @@
+package Practice;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class selfpractice4 {
+  public static void main(String[] args) {
+    /*
+        Scanner sc=new Scanner(System.in);
+
+    System.out.println("1에서 4사이의 숫자를 입력하세요.");
+    int number = sc.nextInt();
+    switch (number){
+        case 1:
+        System.out.println("봄");
+        break;
+        case 2:
+        System.out.println("여름");
+        break;
+        case 3:
+        System.out.println("가을");
+        break;
+        case 4:
+        System.out.println("겨울");
+        break;
+        default:
+        System.out.println("잘못된 입력입니다!");
+        break;
+    }
+    sc.close();
+
+
+    Random random = new Random();
+    Scanner sc = new Scanner(System.in);
+    int randomNum = random.nextInt(100) + 1;
+    System.out.println("1부터 100사이의 숫자 중에서 맞춰보세요.");
+    while (true) {
+      System.out.println("숫자를 입력하세요.");
+      int guessNum = sc.nextInt();
+
+      if (guessNum > randomNum) {
+        System.out.println("더 작은 숫자입니다.");
+      } else if (guessNum < randomNum) {
+        System.out.println("더 큰 숫자입니다.");
+      } else {
+        System.out.println("정답입니다ㅊㅋㅊㅋ");
+        break;
+      }
+      }
+    /*
+
+
+     */
+     */
+    }
+  }
+
