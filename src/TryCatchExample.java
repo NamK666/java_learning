f344a2590c66269620384cd53ad8df7d9c8af9cc
NamK666 <chriscole77@naver.com>
2024-06-15 22:59:16 +0900

    init

---
+++ src/TryCatchExample.java
@@ -0,0 +1,11 @@
+public class TryCatchExample {
+  public static void main(String[] args) {
+    try {
+      int result = 10 / 0;
+      System.out.println(result);
+    } catch (ArithmeticException e) {
+      System.out.println("0으로 나눌 수 없습니다.");
+    }
+    System.out.println("정상적으로 종료되었습니다.");
+  }
+}
