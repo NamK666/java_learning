fdc030dfff3ca428b798a0ceb09aeefa61e871ad
NamK666 <chriscole77@naver.com>
2024-06-24 23:27:24 +0900

    init

---
+++ src/AbstractPractice2/Main.java
@@ -0,0 +1,9 @@
+package AbstractPractice2;
+
+public class Main {
+    public static void main(String[] args){
+        SmartPhone phone = new SmartPhone("Lee");
+
+        phone.turnOn();
+    }
+}
