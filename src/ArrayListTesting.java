0715473f7b7e245e3cba9858636971eb568a353c
NamK666 <chriscole77@naver.com>
2024-06-27 23:23:08 +0900

    init

--- src/ArrayListTesting.java
+++ src/ArrayListTesting.java
@@ -12,5 +12,11 @@ public class ArrayListTesting {
     System.out.println(menu.get(0));
     System.out.println(menu.get(1));
     System.out.println(menu.get(2));
+
+    menu.add("coke");
+    System.out.println(menu);
+
+    menu.set(0, "PIZZA");
+    System.out.println(menu);
   }
 }
