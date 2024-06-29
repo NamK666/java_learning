98a2356e7906dd4afd3cff1742390e7e456810bc
NamK666 <chriscole77@naver.com>
2024-06-29 23:22:11 +0900

    init

---
+++ src/FinalPractice/Constants.java
@@ -0,0 +1,6 @@
+package FinalPractice;
+
+public class Constants {
+    public static final double Gravity = 9.8;
+    public static final int Max_Students = 30;
+}
---
+++ src/FinalPractice/MainConst.java
@@ -0,0 +1,8 @@
+package FinalPractice;
+
+public class MainConst {
+  public static void main(String[] args) {
+    System.out.println(Constants.Max_Students);
+    System.out.println(Constants.Gravity);
+  }
+}
