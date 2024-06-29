0e31ff2ce8d108316d0930382d756b0f111f2ef3
NamK666 <chriscole77@naver.com>
2024-06-29 23:31:06 +0900

    init

---
+++ src/PrivatePractice/MainSecret.java
@@ -0,0 +1,7 @@
+package PrivatePractice;
+
+public class MainSecret {
+    public static void main(String[] args){
+        Secret secret = new Secret("Nam");
+    }
+}
---
+++ src/PrivatePractice/Secret.java
@@ -0,0 +1,13 @@
+package PrivatePractice;
+
+public class Secret {
+    private String name;
+
+    public Secret(String name) {
+        this.name = name;
+    }
+
+    private String getName(){
+        return this.name;
+    }
+}
