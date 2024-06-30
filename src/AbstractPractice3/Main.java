2cb1c303d455b8ced09bad512a94853232a59dc8
NamK666 <chriscole77@naver.com>
2024-06-30 14:57:10 +0900

    init

---
+++ src/AbstractPractice3/Main.java
@@ -0,0 +1,11 @@
+package AbstractPractice3;
+
+public class Main {
+    public static void main(String[] args){
+        SmartPhone smartPhone = new SmartPhone("이남경");
+
+        smartPhone.turnOn();
+        smartPhone.turnOff();
+        smartPhone.internetSearch();
+    }
+}
---
+++ src/AbstractPractice3/SmartPhone.java
@@ -0,0 +1,10 @@
+package AbstractPractice3;
+
+public class SmartPhone extends Phone{
+    public SmartPhone(String owner) {
+        super(owner);
+    }
+    public void internetSearch(){
+    System.out.println("인터넷 서칭을 합니다.");
+    }
+}
