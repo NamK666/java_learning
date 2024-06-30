289d3db3543e05cf2709fc008dced57c89b4f55e
NamK666 <chriscole77@naver.com>
2024-06-30 14:23:33 +0900

    init

---
+++ src/OveridePractice/Child.java
@@ -0,0 +1,11 @@
+package OveridePractice;
+
+public class Child extends Parent {
+    void method2(){
+    System.out.println("Child의 메소드2 호출");
+    }
+
+    void method3(){
+    System.out.println("Child의 메소드3 호출");
+    }
+}
---
+++ src/OveridePractice/Parent.java
@@ -0,0 +1,11 @@
+package OveridePractice;
+
+public class Parent {
+  void method1() {
+    System.out.println("Parent 메소드1 호출");
+  }
+
+  void method2() {
+    System.out.println("Parent 메소드2 호출");
+  }
+}
