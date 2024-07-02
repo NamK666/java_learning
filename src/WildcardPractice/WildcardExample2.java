1c05f309daef34b560abc6caf81ef645214a719e
NamK666 <chriscole77@naver.com>
2024-07-02 22:44:43 +0900

    init

--- src/WildcardPractice/WildcardExample2.java
+++ src/WildcardPractice/WildcardExample2.java
@@ -1,3 +1,22 @@
 package WildcardPractice;
 
-public class WildcardExample2 {}
+import java.util.Arrays;
+import java.util.List;
+
+public class WildcardExample2 {
+  public static double sumOfList(List<? extends Number> list) {
+    double sum = 0.0;
+    for (Number n : list) {
+      sum += n.doubleValue();
+    }
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    List<Integer> intList = Arrays.asList(1, 2, 3);
+    List<Double> doubleList = Arrays.asList(1.1, 2.2, 3.3);
+
+    System.out.println("Sum of intList: " + sumOfList(intList));
+    System.out.println("Sum of doubleList: " + sumOfList(doubleList));
+  }
+}
