c2781c13c7a033b29d2ede815d56bd09ac1e26d2
NamK666 <chriscole77@naver.com>
2024-07-06 18:14:38 +0900

    init

---
+++ src/Lamda/NormalWay.java
@@ -0,0 +1,23 @@
+package Lamda;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class NormalWay {
+  public static void main(String[] args) {
+
+    List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+    Iterator<Integer> iterator = numbers.iterator();
+
+    while (iterator.hasNext()) {
+      Integer number = iterator.next();
+      if(number % 2 == 0) {
+        iterator.remove();
+      }
+    }
+    System.out.println(numbers);
+  }
+}
