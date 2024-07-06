85196f72378251583fadf3522e31eac5916a30c3
NamK666 <chriscole77@naver.com>
2024-07-06 20:23:05 +0900

    init

--- src/StreamPractice/ReducePrac.java
+++ src/StreamPractice/ReducePrac.java
@@ -1,3 +1,13 @@
 package StreamPractice;
 
-public class ReducePrac {}
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ReducePrac {
+    public static void main(String[] args){
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        int sum = list.stream().reduce(0,(a,b)->a+b);
+    }
+}
