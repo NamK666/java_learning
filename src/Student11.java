79ee2230e1f50f99b73e65a540f80cddaef8adde
NamK666 <chriscole77@naver.com>
2024-07-14 23:57:38 +0900

    init

---
+++ src/Student11.java
@@ -0,0 +1,18 @@
+public class Student11 {
+        int level; // 멤버 변수
+
+        // 메서드 (기능)
+        public void levelUp(){
+            level++;
+            System.out.println("레벨이 1증가 했습니다.");
+            System.out.println("현재 레벨은 =" + level +" 입니다.");
+        }
+
+        public void levelDown(){
+            level--;
+            System.out.println("레벨이 1감소 했습니다.");
+            System.out.println("현재 레벨은 =" + level +" 입니다.");
+        }
+    }
+
+
